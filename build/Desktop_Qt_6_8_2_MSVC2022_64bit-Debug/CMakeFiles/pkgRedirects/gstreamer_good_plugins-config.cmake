2e8873817bdd157a57d5627777aff35e6abd852f
sirius2beta <sirius2beta@gmail.com>
2025-03-11 21:20:46 +0800

    dlt build

--- build/Desktop_Qt_6_8_2_MSVC2022_64bit-Debug/CMakeFiles/pkgRedirects/gstreamer_good_plugins-config.cmake
+++
@@ -1,2 +0,0 @@
-include("C:/Project/New_repo/Lighthouse/gstqml6gl/gstreamer_good_plugins-extra.cmake" OPTIONAL)
-include("C:/Project/New_repo/Lighthouse/gstqml6gl/gstreamer_good_pluginsExtra.cmake" OPTIONAL)
